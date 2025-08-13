e779575be12b34c87c668888e896bc168fd2266b
Mona Raj <agrmonaraj@gmail.com>
2025-08-13 11:47:44 +0530

    Initial commit

---
+++ aug13/arr1.cpp
@@ -0,0 +1,109 @@
+// declare an array, initialize it, display it
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//   int arr[5] = {1, 2, 3, 4, 5};
+//   for (int i = 0; i < 5; i++)
+//   {
+//     cout << arr[i] << endl;
+//   }
+//   return 0;
+// }
+
+// declare an array, get values from user, display it
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//   int arr[5];
+//   for (int i = 0; i < 5; i++)
+//   {
+//     cout << "Enter item at index " << i << " : ";
+//     cin >> arr[i];
+//   }
+  
+//   cout << "The array items are : " << endl ;
+//   for (int i = 0; i < 5; i++)
+//   {
+//     cout << arr[i] << "\t";
+//   }
+//   return 0;
+// }
+
+// declare two arrays, get values from user, display them
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//   int arr1[5], arr2[3];
+
+//   cout << "Enter items for array 1 :" <<  endl;
+//   for (int i = 0; i < 5; i++)
+//   {
+//     cout << "Enter item at index " << i << " : ";
+//     cin >> arr1[i];
+//   }
+
+//   cout << "Enter items for array 2 :" <<  endl;
+//   for (int i = 0; i < 3; i++)
+//   {
+//     cout << "Enter item at index " << i << " : ";
+//     cin >> arr2[i];
+//   }
+  
+//   cout << endl << "The first array items are : " << endl ;
+//   for (int i = 0; i < 5; i++)
+//   {
+//     cout << arr1[i] << "\t";
+//   }
+
+//   cout << endl << "The second array items are : " << endl ;
+//   for (int i = 0; i < 3; i++)
+//   {
+//     cout << arr2[i] << "\t";
+//   }
+//   return 0;
+// }
+
+// declare 3 arrays, store the sum of first 2 in 3rd array
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int arr1[5], arr2[5], sum[5];
+
+  cout << "Enter items for array 1 :" <<  endl;
+  for (int i = 0; i < 5; i++)
+  {
+    cout << "Enter item at index " << i << " : ";
+    cin >> arr1[i];
+  }
+  cout << "Enter items for array 2 :" <<  endl;
+  for (int i = 0; i < 5; i++)
+  {
+    cout << "Enter item at index " << i << " : ";
+    cin >> arr2[i];
+  }
+
+  for (int i = 0; i < 5; i++)
+  {
+    sum[i] = arr1[i] + arr2[i];
+  }
+
+  cout << endl << "The sum of array items are : " << endl ;
+  for (int i = 0; i < 5; i++)
+  {
+    cout << sum[i] << "\t";
+  }
+
+  return 0;
+}
